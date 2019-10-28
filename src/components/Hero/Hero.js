01f38b52366d6938207dcb4587264d0ffeb3e5fa
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2019-10-28 10:20:49 +0100

    Add change in Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import styles from './MyComponent.scss';
+import styles from './Hero.scss';
 
-const MyComponent = () => (
+const Hero = () => (
   <header className={styles.component}>
     <h2 className={styles.title}>Things to do</h2>
     <img className={styles.image} src="https://images.pexels.com/photos/1624496/pexels-photo-1624496.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
   </header>
 );
 
-export default MyComponent;
+export default Hero;
