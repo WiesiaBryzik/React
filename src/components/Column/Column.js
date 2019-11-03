5fb9471bde1abab7b4ef6251a4ec35896c042bdc
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2019-11-03 18:48:19 +0100

    Add refaktoring

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,50 +1,35 @@
 import React from 'react';
 import styles from './Column.scss';
+// import { settings } from '../../data/dataStore';
 import Card from '../Card/Card';
-import { settings } from '../../data/dataStore';
-import Creator from '../Creator/Creator';
+// import Creator from '../Creator/Creator';
 import Icon from '../Icon/Icon';
 import PropTypes from 'prop-types';
 
 class Column extends React.Component {
-  state = {
-    cards: this.props.cards || [],
-  }
 
   static propTypes = {
-    cards: PropTypes.node.isRequired,
+    cards: PropTypes.array,
     icon: PropTypes.node,
     title: PropTypes.string.isRequired,
   }
 
-  addCard(title) {
-    this.setState(state => (
-      {
-        cards: [
-          ...state.cards,
-          {
-            key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
-            title,
-          },
-        ],
-      }
-    ));
-  }
-
   render() {
+    const { title, icon, cards } = this.props;
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}><span className={styles.icon}><Icon name={this.props.icon} /></span>{this.props.title}</h3>
+        <h3 className={styles.title}><span className={styles.icon}><Icon name={icon} /></span>{title}</h3>
 
         <div className={styles.columns}>
-          {this.state.cards.map(({ key, ...cardProps }) => (
-            <Card key={key} {...cardProps} />
+          {cards.map(cardData => (
+            <Card key={cardData.id} {...cardData} />
           ))}
         </div>
 
+        {/*
         <div className={styles.creator}>
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-        </div>
+        </div> */}
 
       </section>
     );
