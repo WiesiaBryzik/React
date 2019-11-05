d81b8dbd896ab9f9f3a5b9ee433c382f54647c24
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2019-11-05 13:07:01 +0100

    Add Columns & Carda in React

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Column.scss';
 import Card from '../Card/Card';
-// import Creator from '../Creator/Creator';
+import Creator from '../Creator/Creator';
 import Icon from '../Icon/Icon';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
@@ -12,6 +12,7 @@ class Column extends React.Component {
     cards: PropTypes.array,
     icon: PropTypes.node,
     title: PropTypes.string.isRequired,
+    addCard: PropTypes.func,
   }
 
   static defaultProps = {
@@ -19,7 +20,7 @@ class Column extends React.Component {
   }
 
   render() {
-    const { title, icon, cards } = this.props;
+    const { title, icon, cards, addCard } = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}><span className={styles.icon}><Icon name={icon} /></span>{title}</h3>
@@ -29,11 +30,10 @@ class Column extends React.Component {
             <Card key={cardData.id} {...cardData} />
           ))}
         </div>
-
-        {/*
+        
         <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-        </div> */}
+          <Creator text={settings.cardCreatorText} action={addCard} />
+        </div>
 
       </section>
     );
