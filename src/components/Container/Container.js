11a3e76c342b52670a7d2fbf0799be5cbf50bc64
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2019-11-17 13:26:05 +0100

    Add Container

---
+++ src/components/Container/Container.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Container.scss';
+
+class Container extends React.Component {
+
+  static propTypes = {
+    children: PropTypes.node,
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div className={styles.component}>
+        { children }
+      </div>
+    );
+  }
+}
+
+export default Container;
