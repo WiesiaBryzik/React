c0ead840d93833f947f8efee27ad6f1c519937a9
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2019-11-17 20:12:56 +0100

    Add changes in Container

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,6 +6,7 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import Column from '../Column/ColumnContainer';
 import Creator from '../Creator/Creator';
+import Container from '../Container/Container';
 
 
 class List extends React.Component {
@@ -26,21 +27,22 @@ class List extends React.Component {
     const { title, image, description, columns, addColumn } = this.props;
     return (
       <section className={styles.component}>
+        <Container>
+          <Hero titleText={title} image={image} />
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
 
-        <Hero titleText={title} image={image} />
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
+          <div className={styles.columns}>
+            {columns.map(columnData => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
 
-        <div className={styles.columns}>
-          {columns.map(columnData => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
-
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={addColumn}/>
-        </div>
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn}/>
+          </div>
+        </Container>
       </section>
     );
   }
